e6d9ba85d46351168a31408078ad6b687ef54776
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-06 19:43:58 +0330

    solution to question 6

---
+++ 6.py
@@ -0,0 +1,11 @@
+def sumOfSquares(n):
+	sum = 0
+	for i in range(1, n + 1):
+		sum = sum + (i * i)
+	return sum
+def squareOfSums(n):
+	sum = 0
+	for i in range(1, n + 1):
+		sum = sum + (i)
+	return sum * sum
+print(squareOfSums(100) - sumOfSquares(100))
